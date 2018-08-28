66e0b861659e89dfe240c2d266a7b3e12043d779
“Richard Middleton” <Richard@richardmiddleton.me>
2018-08-28 15:02:01 +0200

    css transitions

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
 
 const stoicapi = require("stoic-api");
 
 class App extends Component {
-
   state = {
-    quote: 'hell'
-  }
+		quote: "",
+		appearQuote: true
+  };
 
   componentDidMount() {
     this.getQuote();
   }
 
   getQuote = () => {
-    let quote = stoicapi.random();
-    this.setState({quote});
-  }
+    const quote = stoicapi.random();
+    this.setState({ quote });
+  };
 
   render() {
     return (
       <div className="App">
-        <div className="wrapper">
-          <h1 className="quote">{this.state.quote}</h1>
-          <button onClick={() => this.getQuote() }></button>
-        </div>
+					<TransitionGroup className="wrapper">
+						<CSSTransition
+							key={uuid()}
+							in={this.state.appearQuote}
+							appear={true}
+							timeout={1000}
+							classNames="fade"
+						>
+							<h1 className="quote">{this.state.quote}</h1>
+						</CSSTransition>
+						<button onClick={() => this.getQuote()} />
+					</TransitionGroup>
       </div>
     );
   }
